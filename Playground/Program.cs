f2446bb95d9f14b4eefa3d528e9cd7589dacb632
PeSul <usrtep@seznam.cz>
2021-11-22 11:23:13 +0100

    //3

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -4,11 +4,32 @@ Console.WriteLine("Hello, World!");
 
 
 var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
 var numInString = strings.Select(x => x.ToUpper());
 
-foreach (var item in numInString)
+PrintList(numInString.ToList());
+
+//2 numbers zjistěte pomocí LINQ zda pole obsahuje pouze suda cisla
+
+//var result2 = numbers.Where(x => x % 2 != 0).Count();
+
+bool isOnlyEvenNumbers = numbers.All(x => x % 2 == 0);
+
+global::System.Console.WriteLine($"Jsou všechna čísla suda: { isOnlyEvenNumbers}");
+
+//3 Vypište čísla v poli jako slova -LINQ
+
+
+var result3 = numbers.Select(x => strings[x]);
+PrintList(result3.ToList());
+
+    static void PrintList(List<string> listToPrint)
 {
-    Console.WriteLine(item);
+    foreach (var item in listToPrint)
+    {
+        Console.WriteLine(item);
+    }
+
 }
